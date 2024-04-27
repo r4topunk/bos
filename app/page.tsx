264e3c6d974eea4696adac265f180ee968bfceff
¨sktbrd¨ <sktbrd.eth@gmail.com>
2024-04-27 14:37:24 -0300

    stamps image, sort of... and fix holder balance logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useEffect, useState } from 'react';
-import { Box, Text, Center, VStack, Table, Thead, Tbody, Tr, Th, Td } from '@chakra-ui/react';
+import { Box, Text, Center, VStack, Table, Thead, Tbody, Tr, Th, Td, Image } from '@chakra-ui/react';
 import axios from 'axios';
 import { stamp_ids } from './stampIDs';
 import { formatBTCaddress } from './formatBTCaddress';
@@ -34,23 +34,30 @@ export default function Home() {
   useEffect(() => {
     const fetchAndAggregate = async () => {
       const promises = stamp_ids.map(id => fetchHolders(id));
+
       const results = await Promise.all(promises);
+      console.log("fetch Holders result", results);
       let total = 0;
 
       setAllHolders(prevState => {
         const newState = { ...prevState };
+        console.log(newState);
         results.forEach((holders: Holder[], index) => {
           const stampId = stamp_ids[index];
           holders.forEach(holder => {
             const { address, quantity } = holder;
             if (!newState[address]) {
-              newState[address] = { total: 0 }; // Initialize with total key
+              newState[address] = { total: 0 };
             }
-            newState[address][stampId] = (newState[address][stampId] || 0) + quantity;
-            newState[address].total += quantity; // Increment total stamps held by the address
-            total += quantity;
+            // Only add quantity if it hasn't been added before for the same stampId
+            if (!newState[address][stampId]) {
+              newState[address][stampId] = quantity;
+              newState[address].total += quantity;
+              total += quantity;
+            }
           });
         });
+
         setNumberOfHolders(Object.keys(newState).length);
         setTotalStamps(total);
         return newState;
@@ -64,34 +71,58 @@ export default function Home() {
 
   const topTenStyle = (index: any) => index < 10 ? { bg: 'yellow.200' } : {};
 
+  const getStampInfo = async (stampId: string): Promise<any> => {
+    const stampInfo = await axios.get(`https://stampchain.io/api/stamps?/${stampId}`);
+    return stampInfo;
+  }
+
+  useEffect(() => {
+    getStampInfo("A4515906628890571300");
+  }
+  )
+
+  function handleImageError(e: any) {
+    e.target.onerror = null; // Prevent infinite loop in case the backup image also fails
+    e.target.src = 'https://stampchain.io/stamps/e16b151228f959a8c91251eec63b390326960c4adbc85b90f1d556454b549e19.png';
+  }
+
   return (
     <Box p={5}>
       <Center>
         <VStack spacing={4} width="100vw">
           <Text fontSize="xl" mb={4}>Total number of holders: {numberOfHolders}</Text>
-          <Box width="100%" overflowX="auto" m={5}>
+          <Box width="100%" overflow="visible" m={5}>
             <Table borderRadius={"10"} variant="simple" size="sm" colorScheme="teal" >
               <Thead border={"1px solid limegreen"}>
                 <Tr>
-                  <Th isNumeric>Address</Th>
+                  <Th border={"1px solid limegreen"} isNumeric>Address</Th>
+                  <Th minW={"80px"} border={"1px solid limegreen"} isNumeric>Total</Th> {/* Moved Total column here */}
                   {stamp_ids.map(id => (
-                    <Th key={id} isNumeric>{id}</Th>
+                    <Th key={id} isNumeric>
+                      <Box width={"120px"}>
+                        <Image
+                          src={`https://assets.stamped.ninja/${id}.png`}
+                          alt={'stamp'}
+                          boxSize="120px"
+                          onError={handleImageError}
+                        />
+                      </Box>
+                    </Th>
                   ))}
-                  <Th isNumeric>Total</Th>
                 </Tr>
               </Thead>
               <Tbody border={"1px solid limegreen"}>
                 {sortedAddresses.map((address, index) => (
                   <Tr key={address} {...topTenStyle(index)} border={"1px solid limegreen"}>
-                    <Td>{formatBTCaddress(address)}</Td>
+                    <Td border={"1px solid limegreen"}>{address}</Td>
+                    <Td border={"1px solid limegreen"} isNumeric> <Center>{allHolders[address].total}</Center></Td> {/* Moved Total cell here */}
                     {stamp_ids.map(id => (
-                      <Td justifyContent={"center"} border={"1px solid limegreen"} key={id} isNumeric>{allHolders[address][id] || 0}</Td>
+                      <Td border={"1px solid limegreen"} key={id} isNumeric><Center>{allHolders[address][id] || 0}</Center></Td>
                     ))}
-                    <Td border={"1px solid limegreen"} isNumeric>{allHolders[address].total}</Td>
                   </Tr>
                 ))}
                 <Tr>
-                  <Td colSpan={stamp_ids.length + 1}><strong>Total Stamps Held:</strong></Td>
+                  <Td colSpan={stamp_ids.length + 2}><strong>Total Stamps Held:</strong></Td>
                   <Td isNumeric>{totalStamps}</Td>
                 </Tr>
               </Tbody>
@@ -101,4 +132,5 @@ export default function Home() {
       </Center>
     </Box>
   );
+
 }
